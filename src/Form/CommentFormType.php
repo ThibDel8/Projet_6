3119119e3f2ae0c75b1c52a91a4589e021a89fdc
Thibault Delattre <delattre.thibault8@gmail.com>
2024-04-18 14:54:03 +0200

    commit part2, forgotten to add new files

---
+++ src/Form/CommentFormType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class CommentFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('commentaire', TextType::class, [
+                'label' => 'Écrire un commentaire :',
+                'constraints' => [
+                    new NotBlank(),
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
