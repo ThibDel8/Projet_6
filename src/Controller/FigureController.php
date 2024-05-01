d76db94942e4665343d72cfc17261dbab04bfda5
Thibault Delattre <delattre.thibault8@gmail.com>
2024-05-01 13:25:50 +0200

    added robots.txt and site.webmanifest files, then add a newline character in figures_list.html.twig

--- src/Controller/FigureController.php
+++ src/Controller/FigureController.php
@@ -168,7 +168,7 @@ class FigureController extends AbstractController
         return $this->redirectToRoute('app_home');
     }
 
-    public function deleteAllPicturesAssocied(Figure $figure)
+    public function deleteAllPicturesAssocied(Figure $figure): void
     {
         $images = $figure->getMedias()->filter(fn (Media $media) => $media->getType() === 'image')->toArray();
 
